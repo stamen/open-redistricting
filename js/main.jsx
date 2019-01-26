932983e3981783f7bbe5f3e8559d69c0932cb8b0
Eric Socolofsky <eric@transmote.com>
2019-01-26 15:23:19 -0800

    WIP: migrate react-router from v3 to v4

--- js/main.jsx
+++ js/main.jsx
@@ -2,15 +2,16 @@ import '@babel/polyfill';
 import React from 'react';	// needed to parse JSX below
 import { render } from 'react-dom';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
-import { routerReducer, syncHistoryWithStore } from 'react-router-redux';
+// import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { BrowserRouter, Route, Switch } from 'react-router';
+// import { routerReducer, syncHistoryWithStore } from 'react-router-redux';
 import { createHashHistory } from 'history';
 
 import App from './views/App.jsx';
-import Auth from './views/Auth.jsx';
-import HomePage from './views/HomePage.jsx';
-import ProjectPage from './views/ProjectPage.jsx';
-import ProposalPage from './views/ProposalPage.jsx';
+// import Auth from './views/Auth.jsx';
+// import HomePage from './views/HomePage.jsx';
+// import ProjectPage from './views/ProjectPage.jsx';
+// import ProposalPage from './views/ProposalPage.jsx';
 import RouteNotFound from './views/404.jsx';
 
 import reducers, { initialState } from './models/reducers';
@@ -26,8 +27,8 @@ auth.init(appConfig.auth[process.env.NODE_ENV === 'production' ? 'prod' : 'dev']
 // Create the single store for this application session
 const store = createStore(
 	combineReducers({
-		...reducers,
-		routing: routerReducer
+		...reducers/*,
+		routing: routerReducer*/
 	}),
 	initialState,
 	applyMiddleware(...middleware)
@@ -38,12 +39,14 @@ const actions = actionCreator(store, transport({
 	expiration: 60000
 }));
 
+/*
 // set up hash history without querystring cruft (e.g. ?_k=xi50sh)
 // from: https://github.com/reactjs/react-router/blob/master/upgrade-guides/v2.0.0.md#using-custom-histories
 const appHistory = useRouterHistory(createHashHistory)({
 	// queryKey: false	// deprecated in 
 });
 syncHistoryWithStore(appHistory, store);
+*/
 
 // Pass the session store and actionCreator into
 // every component created by `react-router`.
@@ -54,6 +57,16 @@ const createReduxComponent = (Component, props) => {
 	return <Component { ...propsWithStore } />;
 };
 
+render((
+	<BrowserRouter createElement={ createReduxComponent }>
+		<Switch>
+			<Route exact path='/' component={ App } />
+			<Route path='*' component={ RouteNotFound } />
+		</Switch>
+	</BrowserRouter>
+), document.getElementById('app'));	
+
+/*
 // Render the app as `react-router` <Route>s, within a <Router>
 render((
 	<Router history={ appHistory } createElement={ createReduxComponent }>
@@ -66,3 +79,4 @@ render((
 		<Route path='*' component={ RouteNotFound } />
 	</Router>
 ), document.getElementById('app'));
+*/
